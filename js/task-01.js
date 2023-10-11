db5de30a7d25deedd654ff7be57383b080d72b60
denisov-vladimir <denisoff@i.ua>
2023-10-11 22:49:56 +0300

    hw6N1
    hw6N1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+
+const listItems = document.querySelectorAll(".item");
+console.log('Number of categories:', listItems.length);
+console.log( listItems);
+listItems.forEach((item) => {
+    const itemName = item.querySelector("h2").textContent;
+    const itemElement = item.querySelectorAll("ul li");
+    console.log(`category: ${itemName}`);
+    console.log(`enlemet: `, itemElement.length);
+})
